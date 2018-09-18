5e4911038b77c0ca48ec12627cc1eb92e0806dff
GloryPassarello <gloria.passarello@virgilio.it>
2018-09-18 22:17:22 +0200

    added data

---
+++ download_grib.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+import ftplib
+import os
+import datetime
+
+data = datetime.datetime.now().isoformat()
+data_split = data.split('T')
+data = data_split[0]
+### type of data used on fow download
+data2 = data.replace("-", "")
+year = data[: 4]
+month = data[5:7]
+day = data[8: ]
+
+input_path = "/home/gloria/the-python-space-adventure/pollutants"
+contaminator = [ 'pm1', 'pm2p5', 'pm10', 'uvbed', 'tcno2',
+                 'tc_no']
+list_hours = ['024', '025', '026','027', '028', '029',
+            '030', '031', '032', '033', '034', '035',
+            '036', '037', '038', '039', '040', '041',
+            '042', '043','044', '045', '046', '047' ]
+
+
+# location of the file in the ftp sever
+path = '/DATA/CAMS_NREALTIME/'+data2+'00/'
+
+
+os.chdir(input_path)
+if not os.path.exists(data2):
+    os.makedirs(data2)
+
+#ftp connection
+ftp = ftplib.FTP("dissemination.ecmwf.int")
+ftp.login("juan.arevalo", "Swd75GxT")
+
+ftp.cwd(path)
+
+# griblist = os.listdir(input_path)
+# for g in griblist:
+#     os.remove(os.path.join(input_path, g))
+
+path_download = "/home/gloria/the-python-space-adventure/pollutants/"+data2
+os.chdir(path_download)
+
+for cont in contaminator:
+    filename = 'z_cams_c_ecmf_'+data2+'000000_prod_fc_sfc_hour_param.grib'
+    indicators = filename.replace("param", cont)
+    for hour in list_hours:
+        hour = str(hour)
+        filename = indicators.replace("hour", hour)
+        ftp.retrbinary("RETR " + filename ,open(filename, 'wb').write)
+        print 'Downloading ', filename, '...'
+
+
+o3_hour = ['024', '027', '030',  '033',
+            '036', '039', '042', '045' ]
+for hour in o3_hour:
+    ozone ='z_cams_c_ecmf_'+data2+'000000_prod_fc_ml_hour_go3.grib'
+    hour = str(hour)
+    filename = ozone.replace("hour", hour)
+    ftp.retrbinary("RETR " + filename ,open(filename, 'wb').write)
+    print 'Downloading ', filename, '...'
+ftp.quit()
